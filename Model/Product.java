a3ba3a1ab4b9ed46ab4e2e02b135d3793b94d4e4
Arthur-Bamberg <bamberguisses@gmail.com>
2022-08-25 22:36:21 -0300

    Product's getters and setters created

--- Model/Product.java
+++ Model/Product.java
@@ -1,5 +1,49 @@
+import utils.Category;
 public class Product {
     String name, description;
     Double price;
     Category category;
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", category=" + category +
+                '}';
+    }
 }
